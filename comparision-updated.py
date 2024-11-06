b3eeadf2fd3c6f44cb523cdca9a251fa2582d3a4
G <2302372@cognizant.com>
2024-11-06 17:48:34 +0530

    new commit

--- comparision-updated.py
+++ comparision-updated.py
@@ -77,10 +77,11 @@ new_df1['Sum of 5xx Error Count'] = new_df1['500 Error Count'] + new_df1['501 Er
 # Drop the '500 Error Count' column from the DataFrame
 new_df1.drop(columns=['500 Error Count', '501 Error Count', '502 Error Count', '503 Error Count'], inplace=True)
 
-new_df1 = pd.merge(new_df1, df2[['Label', '90% Line']], on='Label', how='left')
+
+new_df1 = pd.merge(new_df1, df2[['Label', '90% Line']], on='Label', how='left', validate="many_to_one")
 new_df1.rename(columns={'90% Line': '90% Line Previous Release'}, inplace=True)
 
-new_df1 = pd.merge(new_df1, df1[['Label', '90% Line']], on='Label', how='left')
+new_df1 = pd.merge(new_df1, df1[['Label', '90% Line']], on='Label', how='left', validate="many_to_one")
 new_df1.rename(columns={'90% Line': '90% Line Current Release'}, inplace=True)
 
 new_df1['90% Line Difference'] = new_df1['90% Line Previous Release'] - new_df1['90% Line Current Release']
